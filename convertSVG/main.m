f93e85a864545b7d5fb075b455b38e1bff603222
Noah Spies <nspies@stanford.edu>
2015-03-06 15:45:57 -0800

    Initial Commit

---
+++ convertSVG/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  convertSVG
+//
+//  Created by Noah Spies on 3/6/15.
+//  Copyright (c) 2015 Noah Spies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
